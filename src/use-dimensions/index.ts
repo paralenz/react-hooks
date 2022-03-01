a269d82071dc1a709997fc41815bead5f10f92da
Carsten Daurehøj <arelstone@gmail.com>
2022-03-01 12:34:18 +0100

    🎨 Return ref and resizeObserverRef

--- src/use-dimensions/index.ts
+++ src/use-dimensions/index.ts
@@ -1,33 +1,39 @@
 import { useRef, useState, useEffect, RefObject } from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
 
-type Dimensions = {
-  readonly x: number;
-  readonly y: number;
-  readonly width: number;
-  readonly height: number;
-  readonly top: number;
-  readonly right: number;
-  readonly bottom: number;
-  readonly left: number;
+export type Dimensions = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
 }
 
-export const useDimensions = <T extends Element>(ref: RefObject<T>): Dimensions => {
-  const resizeObserverRef = useRef<ResizeObserver | null>(null)
+export type UseDimensions<T> = Dimensions & {
+  ref: RefObject<T>
+  observer: ResizeObserver | null
+}
 
-  const [dimensions, setdDimensions] = useState<Dimensions>({
-    width: 0,
-    height: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    x: 0,
-    y: 0
-  })
+const initialDimensions: Dimensions = {
+  width: 0,
+  height: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  x: 0,
+  y: 0
+}
+
+export const useDimensions = <T extends Element>(ref: RefObject<T>): UseDimensions<T> => {
+  const resizeObserverRef = useRef<ResizeObserver | null>(null)
+  const [dimensions, setdDimensions] = useState<Dimensions>(initialDimensions)
 
   useEffect(() => {
-    resizeObserverRef.current = new ResizeObserver((entries: Array<ResizeObserverEntry> = []) => {
+    resizeObserverRef.current = new ResizeObserver((entries: ReadonlyArray<ResizeObserverEntry>) => {
       entries.forEach(({ contentRect }) => {
         setdDimensions(contentRect)
       })
@@ -42,5 +48,9 @@ export const useDimensions = <T extends Element>(ref: RefObject<T>): Dimensions 
     }
   }, [ref])
 
-  return dimensions
+  return {
+    ...dimensions,
+    ref,
+    observer: resizeObserverRef.current
+  }
 }
