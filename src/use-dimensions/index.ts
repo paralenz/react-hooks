cd54af4d5a0b432623f192f824bb21a4a78d6956
Carsten Daurehøj <arelstone@gmail.com>
2022-03-01 12:08:03 +0100

    🏷️ Add return type

--- src/use-dimensions/index.ts
+++ src/use-dimensions/index.ts
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect, RefObject } from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
 
-type DOMRectReadOnly = {
+type Dimensions = {
   readonly x: number;
   readonly y: number;
   readonly width: number;
@@ -12,12 +12,18 @@ type DOMRectReadOnly = {
   readonly left: number;
 }
 
-export const useDimensions = <T extends Element>(ref: RefObject<T>): Partial<DOMRectReadOnly> => {
+export const useDimensions = <T extends Element>(ref: RefObject<T>): Dimensions => {
   const resizeObserverRef = useRef<ResizeObserver | null>(null)
 
-  const [dimensions, setdDimensions] = useState<Partial<DOMRectReadOnly>>({
+  const [dimensions, setdDimensions] = useState<Dimensions>({
     width: 0,
-    height: 0
+    height: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    x: 0,
+    y: 0
   })
 
   useEffect(() => {
